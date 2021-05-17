34fe8af0dddc659a4371245b685ca6f88635e58e
spyro21 <38793743+spyro21@users.noreply.github.com>
2021-05-17 12:23:09 -0600

    IDamageable interface

---
+++ summerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IDamageable {
+
+	private float health = 10f;
+	public void takeDamage(float damage) {
+		health -= damage;
+	}
+}
