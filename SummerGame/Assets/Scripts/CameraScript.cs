f04f92d846c46902e624bc5cdcc697df7abce1a3
spyro21 <38793743+spyro21@users.noreply.github.com>
2021-05-18 00:08:26 -0600

    added temporary base movement

---
+++ SummerGame/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    float speed = 4f;
+    private Transform target;
+    private Vector3 distanceFromVec;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //cameraFollowCurrentTarget();
+    }
+
+    void cameraFollowCurrentTarget() {
+        distanceFromVec = transform.position - target.position;
+        float distanceFrom = distanceFromVec.magnitude;
+        if(distanceFrom <= 2) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, (speed / 2) * Time.deltaTime);
+        }  else if( distanceFrom >= 5) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, (speed * 2) * Time.deltaTime);
+        } else {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ SummerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IDamageable
+{
+
+    #region Variables
+
+    public float startingHealth;
+
+    float health;
+
+    Rigidbody2D rb;
+    float movementSpeed = 5f;
+    Vector2 movement;
+    #endregion
+
+    #region Methods
+    public void takeDamage(float damage) { health -= damage; } 
+
+    void Start()
+    {
+        health = startingHealth;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+    #endregion
+
+    #region HelperMethods
+    public Vector2 getMovement() {
+        return movement;
+    }
+    #endregion
+}
